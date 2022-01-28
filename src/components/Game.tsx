9fd0dc97fa25d18f61a55bc97215d70cd211ee9b
Bernardo Lourenço <bernardomiguelourenco@gmail.com>
2022-01-28 11:09:24 +0000

    fixed bug in screen being scrollable

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -106,7 +106,6 @@ export default function Game() {
             display: flex;
             flex-direction: column;
             align-items: stretch;
-            height: 100vh;
           `}
         >
           <div
